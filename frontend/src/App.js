664131fcbce8fcfa168eeef6abbc6d3a526ce73d
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-30 20:38:43 +0530

    login signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSingup from './Pages/LoginSingup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 
 
 
@@ -17,9 +20,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop/>} />
-          <Route path='/mens' element={<ShopCategories category="men" />}/>
-          <Route path='/womens' element={<ShopCategories category="women" />}/>
-          <Route path='/kids' element={<ShopCategories category="kid" />}/>
+          <Route path='/mens' element={<ShopCategories banner={men_banner} category="men" />}/>
+          <Route path='/womens' element={<ShopCategories banner={women_banner} category="women" />}/>
+          <Route path='/kids' element={<ShopCategories banner={kid_banner} category="kid" />}/>
           <Route path='/product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>
