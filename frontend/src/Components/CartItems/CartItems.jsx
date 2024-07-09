efce627f9d99e73ca3e338c7e96072694d241acb
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-07-09 23:19:09 +0530

    total price

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
 
-    const {all_product,cartItems,removeFromCart}=useContext(ShopContext);
+    const {getTotalCartAmount,all_product,cartItems,removeFromCart}=useContext(ShopContext);
   return (
     <div className="cartitems">
         <div className="cartitems-format-main">
@@ -40,7 +40,7 @@ const CartItems = () => {
                 <div>
                     <div className="cartitem-total-item">
                         <p>Subtotal</p>
-                        <p>${0}</p>
+                        <p>${getTotalCartAmount()}</p>
                     </div>
                     <hr />
                     <div className="cartitem-total-item">
@@ -50,14 +50,14 @@ const CartItems = () => {
                     <hr />
                     <div className="cartitem-total-item">
                         <h3>Total</h3>
-                        <h3>${0}</h3>
+                        <h3>${getTotalCartAmount()}</h3>
                     </div>
                 </div>
                 <button>PROCEED TO CHECKOUT</button>
             </div>
             <div className="cartitems-promocode">
                 <p>If you have a promo code, Enter it here</p>
-                <div className="cartitem-promocode">
+                <div className="cartitem-promobox">
                     <input type="text" placeholder="promo code" />
                     <button>Submit</button>
                 </div>
